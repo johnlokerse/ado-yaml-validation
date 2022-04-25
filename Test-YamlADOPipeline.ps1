3ecd23bb3c7c2dd8e1f81e1b49e393fd3130171d
John Lokerse <j.g.lokerse@gmail.com>
2022-04-25 10:48:16 +0200

    Removed unused variables, added PAT param

--- Test-YamlADOPipeline.ps1
+++ Test-YamlADOPipeline.ps1
@@ -20,31 +20,20 @@ param (
                 throw "The file specified in the path must be of type yml."
             }
         })]
-    [string] $YamlFilePath
+    [string] $YamlFilePath,
+
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string] $PersonalAccessToken
 )
 
 
 $Body = @{
     "PreviewRun"   = "true"
     "YamlOverride" = 
-    '
-pool:
-  vmImage: windows-2019
-  
-jobs:
- - job: Run_PowerShell_Write_Host
-   steps:
-     - task: PowerShell@2
-       inputs:
-         targetType: "inline"
-         script: |
-          Write-Host "Hello World"
-'
+    ''
 }
 
-$OrganizationName = "john-lokerse"
-$ProjectName = "Blog"
-$PipelineId = 11
 $Url = "https://dev.azure.com/$OrganizationName/$ProjectName/_apis/pipelines/$PipelineId/runs?api-version=5.1-preview"
 
 $Arguments = @{
@@ -52,7 +41,7 @@ $Arguments = @{
     Uri         = $Url
     Body        = $Body | ConvertTo-Json
     ContentType = "application/json"
-    Headers     = @{Authorization = 'Basic ' + [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":<YOUR_PAT_HERE>")) }
+    Headers     = @{Authorization = 'Basic ' + [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$($PersonalAccessToken)")) }
 }
 
 Invoke-RestMethod @Arguments
